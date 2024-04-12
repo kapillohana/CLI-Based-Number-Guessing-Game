8dc77ce2c21c65b88bff3daa7de8c79a45e75bda
kapillohana <kapillohana79@gmail.com>
2024-04-12 20:16:39 +0500

    pushing to github

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+async function startGame() {
+    const randomNumber = Math.floor(Math.random() * 6 + 1);
+    const answer = await inquirer.prompt([{
+            name: "userGuessedNumber",
+            type: "number",
+            message: "please guess a number between 1 to 6",
+        }]);
+    if (answer.userGuessedNumber === randomNumber) {
+        console.log("Congratulations, you guessed the right number.");
+    }
+    else {
+        console.log("You guessed the wrong answer. The correct number was:", randomNumber);
+    }
+}
+startGame();
